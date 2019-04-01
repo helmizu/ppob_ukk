aa7d9c83f30481a664e6af982036fbb7f339f32a
Helmi <helmi.zulfikar20@gmail.com>
2019-04-01 08:20:53 +0700

    add input and action on tarif

--- src/views/Tarif/Tarif.jsx
+++ src/views/Tarif/Tarif.jsx
@@ -6,19 +6,98 @@ import {
   CardTitle,
   Table,
   Row,
-  Col
+  Col,
+  Button
 } from "reactstrap";
 
-import { PanelHeader } from "components";
+import { PanelHeader, FormInputs } from "components";
 
 import { thead, tbody } from "variables/tarif";
 
 class RegularTables extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      daya: "",
+      harga: ""
+    };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    let data = {
+      daya: this.state.daya,
+      harga: this.state.harga
+    };
+    console.log(data);
+    this.emptyForm();
+  };
+
+  emptyForm = () => {
+    this.setState({
+      daya: "",
+      harga: ""
+    });
+  };
+
   render() {
     return (
       <div>
         <PanelHeader size="sm" />
         <div className="content">
+          <Row>
+            <Col md={12} xs={12}>
+              <Card>
+                <CardHeader>
+                  <CardTitle tag="h4">Tambahkan Tarif</CardTitle>
+                </CardHeader>
+                <CardBody>
+                  <form onSubmit={this.onSubmit}>
+                    <FormInputs
+                      ncols={["col-md-6 pr-1", "col-md-6 pl-1"]}
+                      proprieties={[
+                        {
+                          label: "Daya",
+                          inputProps: {
+                            type: "number",
+                            placeholder: "200",
+                            name: "daya",
+                            onChange: this.onChange,
+                            value: this.state.daya
+                          }
+                        },
+                        {
+                          label: "Harga",
+                          inputProps: {
+                            type: "number",
+                            placeholder: "20000",
+                            name: "harga",
+                            onChange: this.onChange,
+                            value: this.state.harga
+                          }
+                        }
+                      ]}
+                    />
+                    <Button
+                      type="submit"
+                      className="btn-primary float-right mb-4"
+                    >
+                      Submit
+                    </Button>
+                  </form>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
           <Row>
             <Col xs={12}>
               <Card>
@@ -48,7 +127,12 @@ class RegularTables extends React.Component {
                               if (key === thead.length - 1)
                                 return (
                                   <td key={key} className="text-right">
-                                    {prop}
+                                    <a>
+                                      <i
+                                        className="now-ui-icons ui-1_simple-remove text-danger pr-2 cursor-pointer"
+                                        onClick={() => prop}
+                                      />
+                                    </a>
                                   </td>
                                 );
                               return <td key={key}>{prop}</td>;
