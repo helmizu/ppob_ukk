afd19efcfe0d66821b9d9ffd52cb894cb9949f2e
Helmi <helmi.zulfikar20@gmail.com>
2019-04-01 08:52:47 +0700

    add Pembayaran

---
+++ src/variables/pembayaran.jsx
@@ -0,0 +1,56 @@
+const thead = [
+    "#",
+    "Nama",
+    "Tagihan",
+    "Biaya Tagihan",
+    "Biaya Admin",
+    "Total",
+    "Tanggal",
+    "Status",
+    "Verifikasi"
+  ];
+  const tbody = [
+    {
+      className: "",
+      data: [
+        "1",
+        "Helmi Zulfikar",
+        "Januari 2019",
+        "Rp 20000",
+        "Rp 1000",
+        "Rp 21000",
+        "22/02/2019",
+        2,
+        "1"
+      ]
+    }
+  ];
+  
+  const theadPaid = [
+    "#",
+    "Nama",
+    "Tagihan",
+    "Biaya Tagihan",
+    "Biaya Admin",
+    "Total",
+    "Tanggal",
+    "Status"
+  ];
+  const tbodyPaid = [
+    {
+      className: "",
+      data: [
+        "1",
+        "Helmi Zulfikar",
+        "Januari 2019",
+        "Rp 20000",
+        "Rp 1000",
+        "Rp 21000",
+        "22/02/2019",
+        2
+      ]
+    }
+  ];
+
+export { thead, tbody, theadPaid, tbodyPaid };
+  
---
+++ src/views/Pembayaran/Pembayaran.jsx
@@ -0,0 +1,162 @@
+import React from "react";
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  CardTitle,
+  Table,
+  Row,
+  Col
+} from "reactstrap";
+
+import { PanelHeader } from "components";
+
+import { thead, tbody, theadPaid, tbodyPaid } from "variables/pembayaran";
+
+class Pembayaran extends React.Component {
+  render() {
+    return (
+      <div>
+        <PanelHeader size="sm" />
+        <div className="content">
+          <Row>
+            <Col xs={12}>
+              <Card>
+                <CardHeader>
+                  <CardTitle tag="h4">Pembayaran Pending</CardTitle>
+                </CardHeader>
+                <CardBody>
+                  <Table responsive>
+                    <thead className="text-primary">
+                      <tr>
+                        {thead.map((prop, key) => {
+                          if (key === thead.length - 1)
+                            return (
+                              <th key={key} className="text-right">
+                                {prop}
+                              </th>
+                            );
+                          return <th key={key}>{prop}</th>;
+                        })}
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {tbody.map((prop, key) => {
+                        return (
+                          <tr key={key}>
+                            {prop.data.map((prop, key) => {
+                              if (key === thead.length - 1)
+                                return (
+                                  <td key={key} className="text-right">
+                                    <a>
+                                      <i
+                                        className="now-ui-icons ui-1_check text-warning pr-2 cursor-pointer"
+                                        onClick={() => prop}
+                                      />
+                                    </a>
+                                  </td>
+                                );
+                              return <td key={key}>{prop}</td>;
+                            })}
+                          </tr>
+                        );
+                      })}
+                    </tbody>
+                  </Table>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12}>
+              <Card>
+                <CardHeader>
+                  <CardTitle tag="h4">Pembayaran Sukses</CardTitle>
+                </CardHeader>
+                <CardBody>
+                  <Table responsive>
+                    <thead className="text-success">
+                      <tr>
+                        {theadPaid.map((prop, key) => {
+                          if (key === theadPaid.length - 1)
+                            return (
+                              <th key={key} className="text-right">
+                                {prop}
+                              </th>
+                            );
+                          return <th key={key}>{prop}</th>;
+                        })}
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {tbodyPaid.map((prop, key) => {
+                        return (
+                          <tr key={key}>
+                            {prop.data.map((prop, key) => {
+                              if (key === theadPaid.length - 1)
+                                return (
+                                  <td key={key} className="text-right">
+                                    {prop}
+                                  </td>
+                                );
+                              return <td key={key}>{prop}</td>;
+                            })}
+                          </tr>
+                        );
+                      })}
+                    </tbody>
+                  </Table>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12}>
+              <Card>
+                <CardHeader>
+                  <CardTitle tag="h4">Pembayaran Gagal</CardTitle>
+                </CardHeader>
+                <CardBody>
+                  <Table responsive>
+                    <thead className="text-danger">
+                      <tr>
+                        {theadPaid.map((prop, key) => {
+                          if (key === theadPaid.length - 1)
+                            return (
+                              <th key={key} className="text-right">
+                                {prop}
+                              </th>
+                            );
+                          return <th key={key}>{prop}</th>;
+                        })}
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {tbodyPaid.map((prop, key) => {
+                        return (
+                          <tr key={key}>
+                            {prop.data.map((prop, key) => {
+                              if (key === theadPaid.length - 1)
+                                return (
+                                  <td key={key} className="text-right">
+                                    {prop}
+                                  </td>
+                                );
+                              return <td key={key}>{prop}</td>;
+                            })}
+                          </tr>
+                        );
+                      })}
+                    </tbody>
+                  </Table>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Pembayaran;
