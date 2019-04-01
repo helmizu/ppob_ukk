36247954dfedef4fe8a68007e06d7324f50133ca
Helmi <helmi.zulfikar20@gmail.com>
2019-04-01 08:19:04 +0700

    after add tarif table

---
+++ src/variables/tarif.jsx
@@ -0,0 +1,32 @@
+
+  const thead = ["ID", "Daya", "Tarif/KWh", "Action"];
+  const tbody = [
+    {
+      className: "table-success",
+      data: ["Dakota Rice", "Niger", "$36,738", "Oud-Turnhout"]
+    },
+    {
+      className: "",
+      data: ["Minerva Hooper", "Curaçao", "$23,789", "Sinaai-Waas"]
+    },
+    {
+      className: "table-info",
+      data: ["Sage Rodriguez", "Netherlands", "$56,142", "Baileux"]
+    },
+    {
+      className: "",
+      data: ["Philip Chaney", "Korea, South", "$38,735", "Overland Park"]
+    },
+    {
+      className: "table-danger",
+      data: ["Doris Greene", "Malawi", "$63,542", "Feldkirchen in Kärnten"]
+    },
+    { className: "", data: ["Mason Porter", "Chile", "$78,615", "Gloucester"] },
+    {
+      className: "table-warning",
+      data: ["Jon Porter", "Portugal", "$98,615", "Gloucester"]
+    }
+  ];
+  
+  export { thead, tbody };
+  
